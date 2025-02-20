798a9ed3bfad3b5f8d86793da71c557e487ee19d
Tam Vo <tam.vo@cloud-kinetics.com>
2025-02-20 17:49:33 +0700

    update: update ec2 small

--- 02-ec2-small.tf
+++ 02-ec2-small.tf
@@ -1,7 +1,7 @@
 resource "aws_instance" "small_ec2" {
 
   ami                         = var.ec2_ami
-  instance_type               = "t2.medium"
+  instance_type               = "t2.small"
   monitoring                  = true
   subnet_id                   = values(aws_subnet.public_subnets)[0].id
   vpc_security_group_ids      = [aws_security_group.security_group.id]
@@ -10,7 +10,7 @@ resource "aws_instance" "small_ec2" {
   tags = {
     Terraform   = "true"
     Environment = "dev"
-    Name        = "EC2 Medium Server"
+    Name        = "EC2 Small Server"
   }
 
   root_block_device {
